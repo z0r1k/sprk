7161aba26ae15bbb2d15d76103b8daa477fd2ea7
Mykhailo Lieibenson <mykhailo@geeny.io>
2018-07-09 15:10:26 +0200

    data import script

---
+++ app/data/import.js
@@ -0,0 +1,9 @@
+﻿﻿try {
+  const data = JSON.parse(cat('/data/matches.json'))
+  const db = new Mongo().getDB('spark')
+
+  db.createCollection('matches')
+  db.matches.insert(data.matches)
+
+﻿  print('Matches imported', data.matches.length)
+} catch(e) { print(e) }
